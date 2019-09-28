ecb95e23551fec7dfdaf23d0c1926d9681f1f887
Nikola Raicic <nikola.raicic@hotmail.com>
2019-09-28 19:55:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,9 @@ def monopoly_with_fourth_tier
     monopoly = {
     :railroads => {
       :names => {
-        :reading_railroad => "$100",
+        :reading_railroad => {
+          :mortgage_value => "$100"
+        },
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
